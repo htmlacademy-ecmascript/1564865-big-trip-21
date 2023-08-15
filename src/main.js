ebd63a94c56dedc8d8931d004b6c26f978e902e0
Abramof Andrey <abramofandrey2@gmail.com>
2023-08-15 19:58:13 +0300

    добавил class AddButtonView и class SortView

--- src/main.js
+++ src/main.js
@@ -1,14 +1,33 @@
 import './views/brief-view.js';
 import './views/filter-view.js';
+import './views/add-button-view.js';
+import './views/sort-view.js';
 
 /**
  * @type {import('./views/brief-view').default}
  */
+
+const briefView = document.querySelector('brief-view');
+
 /**
  * @type {import('./views/filter-view').default}
  */
 
-const briefView = document.querySelector('brief-view');
 const filterView = document.querySelector('filter-view');
+
+/**
+ * @type {import('./views/add-button-view').default}
+ */
+
+const addButtonView = document.querySelector('add-button-view');
+
+/**
+ * @type {import('./views/sort-view').default}
+*/
+
+const sortView = document.querySelector('sort-view');
+
 briefView.render();
 filterView.render();
+addButtonView.render();
+sortView.render();
