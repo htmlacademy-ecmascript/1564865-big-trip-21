adf21d6d92385df617287d8203f40a8b21c7a561
Abramof Andrey <abramofandrey2@gmail.com>
2023-09-07 00:14:17 +0300

    отредактировал подставление данных в FilterView

--- src/views/filter-view.js
+++ src/views/filter-view.js
@@ -30,17 +30,17 @@ class FilterView extends View {
       ${this.state.items.map((item) => html`
         <div class="trip-filters__filter">
           <input
-            id="filter-everything"
-            class="trip-filters__filter-input
-            visually-hidden"
+            id="filter-${item.value}"
+            class="trip-filters__filter-input  visually-hidden"
             type="radio"
             name="trip-filter"
-            value="everything"
-            checked="">
+            value="${item.value}"
+            ${item.isSelected ? 'checked' : ''}
+            ${item.isDisabled ? 'disabled' : ''}>
           <label
             class="trip-filters__filter-label"
-            for="filter-everything">
-            Everything
+            for="filter-${item.value}">
+            ${item.value}
           </label>
         </div>
       `)}
