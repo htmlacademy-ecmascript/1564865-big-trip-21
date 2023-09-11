9c3986b8597e33d570cb3543189fbb964758536e
Abramof Andrey <abramofandrey2@gmail.com>
2023-09-11 23:43:43 +0300

    реализовал в CardView отправку события favorite

--- src/views/card-view.js
+++ src/views/card-view.js
@@ -12,7 +12,6 @@ class CardView extends View {
     super();
 
     this.addEventListener('click', this.onClick);
-
   }
 
   /**
@@ -135,7 +134,7 @@ class CardView extends View {
     const { isFavorite } = this.state;
 
     return html`
-      <button class="event__favorite-btn ${isFavorite ? ' event__favorite-btn--active' : ''}" type="button">
+      <button class="event__favorite-btn ${isFavorite ? 'event__favorite-btn--active' : ''}" type="button">
         <span class="visually-hidden">Add to favorite</span>
           <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
             <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"></path>
@@ -163,6 +162,11 @@ class CardView extends View {
   onClick(event) {
     if (event.target.closest('.event__rollup-btn')) {
       this.dispatch('open');
+      return;
+    }
+
+    if (event.target.closest('.event__favorite-btn')) {
+      this.dispatch('favorite');
     }
   }
 }
