1d112faedbe13cc74d710cb8bcba16a624b9692d
Abramof Andrey <abramofandrey2@gmail.com>
2023-09-26 23:40:36 +0300

    реализовал смену состояния кнопки Delete

--- src/views/editor-view.js
+++ src/views/editor-view.js
@@ -206,7 +206,7 @@ class EditorView extends View {
    * @returns {string}
    */
   createResetButtonHtml() {
-    const { id } = this.state;
+    const { id, isDeleting } = this.state;
 
     if (id === 'draft') {
       return html`
@@ -214,7 +214,12 @@ class EditorView extends View {
       `;
     }
     return html`
-      <button class="event__reset-btn" type="reset">Delete</button>
+      <button
+        class="event__reset-btn  btn"
+        type="reset"
+        ${isDeleting ? 'disabled' : ''}>
+        ${isDeleting ? 'Deleting...' : 'Delete'}
+      </button>
     `;
   }
 
